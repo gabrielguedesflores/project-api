6cf3d35b153068cacfd543c8ff255917286c8640
Gabriel Guedes Flores <gabrielguedesflores@gmail.com>
2021-10-26 17:07:20 -0300

    upload project

---
+++ public_html/index.php
@@ -0,0 +1,36 @@
+<?php 
+header('Content-Type: application/json');
+require_once '../vendor/autoload.php';
+
+if(isset($_GET['url'])){
+    $url = explode("/", $_GET['url']);
+
+    if($url[0] === 'api' ){
+        array_shift($url); //remove a primeira posição do array 
+        $service = 'App\Services\\'.ucfirst($url[0]) . 'Service';
+        array_shift($url);
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+       
+        try {
+            $response = call_user_func_array(array(new $service, $method), $url);
+            http_response_code(200);
+
+            echo json_encode(array(
+                'Status' => 'success',
+                'Data'    => $response
+            ));
+            exit;
+
+        } catch (\Exception $e) {
+            http_response_code(404);
+            
+            echo json_encode(array(
+                'Status' => 'error',
+                'Data'    => $e->getMessage(),
+            ), JSON_UNESCAPED_UNICODE);
+            exit;
+        }
+    }
+}
+
+//var_dump($url);
